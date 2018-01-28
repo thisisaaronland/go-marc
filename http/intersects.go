80e9c197f248cf82ccc0364c659e9fe883508ff2
thisisaaronland <straup@gmail.com>
2018-01-27 22:42:03 -0800

    start fleshing out uploads (again...)

--- http/intersects.go
+++ http/intersects.go
@@ -1,16 +1,24 @@
 package http
 
 import (
-	_ "encoding/csv"
-	"github.com/thisisaaronland/go-marc/fields"
+       _ "github.com/thisisaaronland/go-marc/fields"
 	_ "github.com/whosonfirst/go-sanitize"
-	_ "log"
+	"log"
 	gohttp "net/http"
 )
 
 func IntersectsHandler() (gohttp.Handler, error) {
 
 	fn := func(rsp gohttp.ResponseWriter, req *gohttp.Request) {
+
+		fh, _, err := req.FormFile("upload")
+
+		if err != nil {
+			gohttp.Error(rsp, err.Error(), gohttp.StatusInternalServerError)
+			return
+		}
+
+		log.Println(fh)
 
 	}
 
